057832e153f541b68ec94eac4c79530ef9e6f33a
Richard Torruellas <richard.torruellas@sony.com>
2023-04-12 15:54:24 -0700

    clean up

--- index.mjs
+++ index.mjs
@@ -6,29 +6,13 @@ import path from "node:path";
 import { spawn } from "node:child_process";
 import { parseArgsStringToArgv } from "string-argv";
 
-const __dirname = process.cwd();
+const workingDirectory = process.cwd();
 
 fs.readFile(path.resolve(process.cwd(), "package.json"), "UTF-8", callback);
 
-async function run(command) {
-  return new Promise((resolve) => {
-    let args = parseArgsStringToArgv(command);
-    let cmd = args.shift();
-
-    let step = spawn(cmd, args, { cwd: process.cwd() });
-
-    step.stdout.pipe(process.stdout);
-    step.stderr.pipe(process.stderr);
-
-    step.on("close", (code) => {
-      resolve(code);
-    });
-  });
-}
-
 function callback(err, _data) {
   if (err) {
-    return console.error("There is no package.json at " + __dirname);
+    return console.error("There is no package.json at " + workingDirectory);
   }
 
   let data;
@@ -36,12 +20,12 @@ function callback(err, _data) {
   try {
     data = JSON.parse(_data);
   } catch (e) {
-    console.error(`The package.json at ${__dirname} has invalid json`);
+    console.error(`The package.json at ${workingDirectory} has invalid json`);
   }
 
   const scripts = data.scripts;
   if (!scripts) {
-    return new Error(`package.json at path ${__dirname} has no scripts`);
+    return new Error(`package.json at path ${workingDirectory} has no scripts`);
   }
 
   const keys = Object.keys(scripts);
@@ -56,6 +40,12 @@ function callback(err, _data) {
       },
     ])
     .then((answers) => {
-      run(`npm run ${answers.script}`);
+      let args = parseArgsStringToArgv(`npm run ${answers.script}`);
+      let cmd = args.shift();
+
+      let step = spawn(cmd, args, { cwd: workingDirectory });
+
+      step.stdout.pipe(process.stdout);
+      step.stderr.pipe(process.stderr);
     });
 }
