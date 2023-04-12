50ba214b953572cb8189c2a64f0aa9e4a5740ac0
Richard Torruellas <richard.torruellas@sony.com>
2023-04-12 13:18:04 -0700

    use spawn over exec

--- index.mjs
+++ index.mjs
@@ -3,12 +3,28 @@
 import inquirer from "inquirer";
 import fs from "node:fs";
 import path from "node:path";
-import { exec } from "node:child_process";
+import { spawn } from "node:child_process";
+import { parseArgsStringToArgv } from "string-argv";
 
 const __dirname = process.cwd();
 
-console.log(__dirname);
 fs.readFile(path.resolve(process.cwd(), "package.json"), "UTF-8", callback);
+
+async function run(command) {
+  return new Promise((resolve) => {
+    let args = parseArgsStringToArgv(command);
+    let cmd = args.shift();
+
+    let step = spawn(cmd, args);
+
+    step.stdout.pipe(process.stdout);
+    step.stderr.pipe(process.stderr);
+
+    step.on("close", (code) => {
+      resolve(code);
+    });
+  });
+}
 
 function callback(err, _data) {
   if (err) {
@@ -40,8 +56,6 @@ function callback(err, _data) {
       },
     ])
     .then((answers) => {
-      exec(scripts[answers.script], function (err, stdout, stderr) {
-        console.log(stdout);
-      });
+      run(scripts[answers.script]);
     });
 }
